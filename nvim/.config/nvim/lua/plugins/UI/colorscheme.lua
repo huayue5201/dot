b6430242f7e2a5e203ca22f38c668e39a1a0e7c5
huayue <369230782@qq.com>
2023-07-18 22:35:58 +0800

    nvim

--- nvim/.config/nvim/lua/plugins/UI/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/UI/colorscheme.lua
@@ -1,50 +1,15 @@
--- https://github.com/rebelot/kanagawa.nvim
+-- https://github.com/projekt0n/github-nvim-theme
 -- 主题配置
 
 return {
-	"rebelot/kanagawa.nvim",
+	"projekt0n/github-nvim-theme",
 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
-		require("kanagawa").setup({
-			compile = true, -- enable compiling the colorscheme
-			undercurl = true, -- enable undercurls
-			commentStyle = { italic = true },
-			functionStyle = {},
-			keywordStyle = { italic = true },
-			statementStyle = { bold = true },
-			typeStyle = {},
-			transparent = false, -- do not set background color
-			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-			terminalColors = true, -- define vim.g.terminal_color_{0,17}
-			colors = { -- add/modify theme and palette colors
-				palette = {},
-				theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-			},
-			overrides = function(colors)
-				local theme = colors.theme
-				return {
-					NormalFloat = { bg = "none" },
-					FloatBorder = { bg = "none" },
-					FloatTitle = { bg = "none" },
-
-					-- Save an hlgroup with dark background and dimmed foreground
-					-- so that you can use it where your still want darker windows.
-					-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-					NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-
-					-- Popular plugins that open floats will link to NormalFloat by default;
-					-- set their background accordingly if you wish to keep them dark and borderless
-					LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-					MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-				}
-			end,
-			theme = "wave", -- Load "wave" theme when 'background' option is not set
-			background = { -- map the value of 'background' option to a theme
-				dark = "wave", -- try "dragon" !
-				light = "lotus",
-			},
+		require("github-theme").setup({
+			-- ...
 		})
-		vim.cmd("colorscheme kanagawa")
+
+		vim.cmd("colorscheme github_dark")
 	end,
 }
