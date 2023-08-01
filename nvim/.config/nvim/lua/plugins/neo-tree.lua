9687ddcd680b1cff394384eae1108c89642de72a
huayue <369230782@qq.com>
2023-08-02 04:16:25 +0800

    neovim

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -151,10 +151,29 @@ return {
 			},
 		},
 		filesystem = {
-			-- 使用系统默认程序打开文件
+			use_libuv_file_watcher = true, -- This will use the OS level file watchers to detect changes
+			-- instead of relying on nvim autocmd events.
 			window = {
 				mappings = {
+					-- 使用系统默认程序打开文件
 					["o"] = "system_open",
+					["<bs>"] = "navigate_up",
+					["."] = "set_root",
+					["<c-h>"] = "toggle_hidden",
+					["/"] = "fuzzy_finder",
+					["D"] = "fuzzy_finder_directory",
+					["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
+					-- ["D"] = "fuzzy_sorter_directory",
+					["f"] = "filter_on_submit",
+					["<c-x>"] = "clear_filter",
+					["[g"] = "prev_git_modified",
+					["]g"] = "next_git_modified",
+				},
+				fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+					["<down>"] = "move_cursor_down",
+					["<C-n>"] = "move_cursor_down",
+					["<up>"] = "move_cursor_up",
+					["<C-p>"] = "move_cursor_up",
 				},
 			},
 			commands = {
