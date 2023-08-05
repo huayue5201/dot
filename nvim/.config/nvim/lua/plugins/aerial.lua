374118bdba0b2c9d776bf607751418289ff37bfe
huayue <369230782@qq.com>
2023-08-05 11:20:47 +0800

    nvim

--- nvim/.config/nvim/lua/plugins/aerial.lua
+++ nvim/.config/nvim/lua/plugins/aerial.lua
@@ -10,6 +10,28 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
+		-- 颜色高亮
+		vim.cmd([[
+			hi link AerialClass Type
+			hi link AerialClassIcon Special
+			hi link AerialFunction Special
+			hi AerialFunctionIcon guifg=#cb4b16 guibg=NONE guisp=NONE gui=NONE cterm=NONE
+			  
+			" There's also this group for the fallback of the text if a specific
+			" class highlight isn't defined
+			hi link AerialNormal Normal
+			" There's also this group for the cursor position
+			hi link AerialLine QuickFixLine
+			" If highlight_mode="split_width", you can set a separate color for the
+			" non-current location highlight
+			hi AerialLineNC guibg=Gray
+			  
+			" You can customize the guides (if show_guide=true)
+			hi link AerialGuide Comment
+			" You can set a different guide color for each level
+			hi AerialGuide1 guifg=Red
+			hi AerialGuide2 guifg=Blue
+      ]])
 		require("aerial").setup({
 			-- Priority list of preferred backends for aerial.
 			-- This can be a filetype map (see :help aerial-filetype-map)
