c9f53b16c02f33c012d6fdbc561b2d87df3d4991
huayue <369230782@qq.com>
2023-09-21 08:57:03 +0800

    nvim

--- nvim/.config/nvim/lua/plugins/persisted.lua
+++ nvim/.config/nvim/lua/plugins/persisted.lua
@@ -6,5 +6,10 @@ return {
 		{ "<leader>wi", "<cmd>SessionSave<cr>", desc = "保存会话" },
 		{ "<leader>wo", "<cmd>SessionLoad<cr>", desc = "恢复会话" },
 	},
-	config = true,
+	opts = {
+      -- git
+		use_git_branch = true,
+		-- 自动保存会话
+		autosave = false,
+	},
 }
