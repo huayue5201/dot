7e9f003258536363c524fb1c642427655a32b702
huayue <369230782@qq.com>
2024-02-22 14:58:50 +0800

    nvim 1.0

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -15,9 +15,31 @@ return {
 		{ "<leader>fw", desc = "检索光标下的字符" },
 	},
 	config = function()
+		local previewers = require("telescope.previewers")
+		local Job = require("plenary.job")
+		local new_maker = function(filepath, bufnr, opts)
+			filepath = vim.fn.expand(filepath)
+			Job:new({
+				command = "file",
+				args = { "--mime-type", "-b", filepath },
+				on_exit = function(j)
+					local mime_type = vim.split(j:result()[1], "/")[1]
+					if mime_type == "text" then
+						previewers.buffer_previewer_maker(filepath, bufnr, opts)
+					else
+						-- maybe we want to write something to the buffer here
+						vim.schedule(function()
+							vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
+						end)
+					end
+				end,
+			}):sync()
+		end
+
 		local actions = require("telescope.actions")
 		require("telescope").setup({
 			defaults = {
+				buffer_previewer_maker = new_maker, -- 不要预览二进制文件
 				-- Default configuration for telescope goes here:
 				-- config_key = value,
 				mappings = {
