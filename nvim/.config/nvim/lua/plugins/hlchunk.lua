7e254750019b0101f2716b52b9571506cfa260d8
huayue <369230782@qq.com>
2024-04-09 15:31:11 +0800

    nvim

--- nvim/.config/nvim/lua/plugins/hlchunk.lua
+++
@@ -1,9 +0,0 @@
--- https://github.com/shellRaining/hlchunk.nvim/blob/main/README.zh-CN.md
-
-return {
-	"shellRaining/hlchunk.nvim",
-	event = { "UIEnter" },
-	config = function()
-		require("hlchunk").setup({})
-	end,
-}
