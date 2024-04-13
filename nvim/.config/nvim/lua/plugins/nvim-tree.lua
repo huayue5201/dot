aabf2e75831bfd9b029ddaf67cde67c11529ac07
huayue <369230782@qq.com>
2024-04-13 19:22:30 +0800

    nvim

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -47,56 +47,30 @@ return {
 		keymap("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { desc = "文件树" })
 
 		-- 当 nvim-tree 是最后一个窗口时自动关闭
-		local function tab_win_closed(winnr)
-			local api = require("nvim-tree.api")
-			local tabnr = vim.api.nvim_win_get_tabpage(winnr)
-			local bufnr = vim.api.nvim_win_get_buf(winnr)
-			local buf_info = vim.fn.getbufinfo(bufnr)[1]
-			local tab_wins = vim.tbl_filter(function(w)
-				return w ~= winnr
-			end, vim.api.nvim_tabpage_list_wins(tabnr))
-			local tab_bufs = vim.tbl_map(vim.api.nvim_win_get_buf, tab_wins)
-
-			if buf_info.name:match(".*NvimTree_%d*$") then -- 关闭的是 nvim tree
-				-- 在 :q 时关闭所有 nvim tree
-				if not vim.tbl_isempty(tab_bufs) then -- 并且不是最后一个窗口（不会被下面的代码自动关闭）
-					api.tree.close()
-				end
-			else -- 关闭的是普通的 buffer
-				if #tab_bufs == 1 then -- 如果标签页中只有一个 buffer
-					local last_buf_info = vim.fn.getbufinfo(tab_bufs[1])[1]
-					if last_buf_info.name:match(".*NvimTree_%d*$") then -- 并且那个 buffer 是 nvim tree
-						vim.schedule(function()
-							if #vim.api.nvim_list_wins() > 1 then -- 如果还有其他窗口
-								vim.api.nvim_win_close(tab_wins[1], true) -- 只关闭nvim-tree窗口
-							else
-								api.tree.close() -- 如果是最后一个窗口，则关闭整个 nvim tree
-							end
-						end)
+		vim.api.nvim_create_autocmd("QuitPre", {
+			callback = function()
+				local invalid_win = {}
+				local wins = vim.api.nvim_list_wins()
+				for _, w in ipairs(wins) do
+					local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+					if bufname:match("NvimTree_") ~= nil then
+						table.insert(invalid_win, w)
 					end
 				end
-			end
-		end
-		vim.api.nvim_create_autocmd("WinClosed", {
-			callback = function()
-				local amatch_value = vim.fn.expand("<amatch>")
-				if amatch_value and amatch_value ~= "" then
-					local winnr = tonumber(amatch_value)
-					if winnr then
-						vim.schedule_wrap(function()
-							tab_win_closed(winnr)
-						end)
+				if #invalid_win == #wins - 1 then
+					-- Should quit, so we close all invalid windows.
+					for _, w in ipairs(invalid_win) do
+						vim.api.nvim_win_close(w, true)
 					end
 				end
 			end,
-			nested = true,
 		})
 
 		-- 打开文件自动关闭nvim-tree窗口
 		vim.api.nvim_create_autocmd("BufLeave", {
 			callback = function()
 				if vim.bo.filetype == "NvimTree" then
-					vim.cmd("q")
+					vim.cmd("NvimTreeClose")
 				end
 			end,
 		})
