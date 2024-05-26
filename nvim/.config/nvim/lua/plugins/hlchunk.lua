b1d8ba3dcc902935d0f70e5db85c692c269ddc21
huayue <369230782@qq.com>
2024-05-26 21:19:39 +0800

    nvim

--- nvim/.config/nvim/lua/plugins/hlchunk.lua
+++ nvim/.config/nvim/lua/plugins/hlchunk.lua
@@ -5,24 +5,11 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		require("hlchunk").setup({
-			default_conf = {
-				enable = false,
-				style = {},
-				notify = false,
-				priority = 0,
-				exclude_filetypes = {
-					aerial = true,
-					dashboard = true,
-					-- some other filetypes
-				},
-			},
 			chunk = {
 				enable = true,
-				-- ...
 			},
 			indent = {
 				enable = true,
-				-- ...
 			},
 			line_num = {
 				enable = true,
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -10,12 +10,51 @@ return {
 	keys = {
 		{ "<leader>ff", desc = "文件检索" },
 		{ "<leader>fg", desc = "字符检索" },
-		{ "<leader>fb", desc = "buffer检索" },
+		-- { "<leader>fb", desc = "buffer检索" },
 		{ "<leader>fw", desc = "光标下word检索" },
 		{ "<leader>fo", desc = "历史文件检索" },
 	},
 	config = function()
+		local previewers = require("telescope.previewers")
+		local Job = require("plenary.job")
+		local new_maker = function(filepath, bufnr, opts)
+			filepath = vim.fn.expand(filepath)
+			Job:new({
+				command = "file",
+				args = { "--mime-type", "-b", filepath },
+				on_exit = function(j)
+					local mime_type = vim.split(j:result()[1], "/")[1]
+					if mime_type == "text" then
+						previewers.buffer_previewer_maker(filepath, bufnr, opts)
+					else
+						-- maybe we want to write something to the buffer here
+						vim.schedule(function()
+							vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
+						end)
+					end
+				end,
+			}):sync()
+		end
+
+		local actions = require("telescope.actions")
 		require("telescope").setup({
+			defaults = {
+				-- 不要预览二进制文件
+				buffer_previewer_maker = new_maker,
+				preview = {
+					-- 忽略大于阈值的文件
+					filesize_limit = 0.1, -- MB
+				},
+			},
+			pickers = {
+				buffers = {
+					mappings = {
+						i = {
+							["<c-d>"] = actions.delete_buffer + actions.move_to_top,
+						},
+					},
+				},
+			},
 			extensions = {
 				fzf = {
 					fuzzy = true, -- false will only do exact matching
