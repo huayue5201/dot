faa6a32cd438cbb541ed2462d07754642208b1ba
huayue <369230782@qq.com>
2024-06-01 14:56:02 +0800

    nvim

--- nvim/.config/nvim/lua/user/completion_icon.lua
+++ nvim/.config/nvim/lua/user/completion_icon.lua
@@ -1,35 +1,36 @@
+-- 参考文档:https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization
+
 local M = {}
 
 M.icons = {
-  Class = " ",
-  Color = " ",
-  Constant = " ",
-  Constructor = " ",
-  Enum = " ",
-  EnumMember = " ",
-  Field = "󰄶 ",
-  File = " ",
-  Folder = " ",
-  Function = " ",
-  Interface = "󰜰",
-  Keyword = "󰌆 ",
-  Method = "ƒ ",
-  Module = "󰏗 ",
-  Property = " ",
-  Snippet = "󰘍 ",
-  Struct = " ",
-  Text = " ",
-  Unit = " ",
-  Value = "󰎠 ",
-  Variable = " ",
+	Class = " ",
+	Color = " ",
+	Constant = " ",
+	Constructor = " ",
+	Enum = " ",
+	EnumMember = " ",
+	Field = "󰄶 ",
+	File = " ",
+	Folder = " ",
+	Function = " ",
+	Interface = "󰜰",
+	Keyword = "󰌆 ",
+	Method = "ƒ ",
+	Module = "󰏗 ",
+	Property = " ",
+	Snippet = "󰘍 ",
+	Struct = " ",
+	Text = " ",
+	Unit = " ",
+	Value = "󰎠 ",
+	Variable = " ",
 }
 
-
 function M.setup()
-  local kinds = vim.lsp.protocol.CompletionItemKind
-  for i, kind in ipairs(kinds) do
-    kinds[i] = M.icons[kind] or kind
-  end
+	local kinds = vim.lsp.protocol.CompletionItemKind
+	for i, kind in ipairs(kinds) do
+		kinds[i] = M.icons[kind] or kind
+	end
 end
 
 return M
