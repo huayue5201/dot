043274add91394e03f009468032cf6e33e2eb690
huayue <369230782@qq.com>
2024-06-03 14:04:06 +0800

    nvim

---
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,23 @@
+-- https://github.com/folke/todo-comments.nvim
+
+return {
+	"folke/todo-comments.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim",
+	},
+	keys = { "<leader>to" },
+	config = function()
+		require("todo-comments").setup()
+
+		vim.keymap.set("n", "<leader>to", "<cmd>TodoTelescope<cr>")
+
+		vim.keymap.set("n", "]t", function()
+			require("todo-comments").jump_next()
+		end, { desc = "Next todo comment" })
+
+		vim.keymap.set("n", "[t", function()
+			require("todo-comments").jump_prev()
+		end, { desc = "Previous todo comment" })
+	end,
+}
