6a60acfb99e53a261379d7849b8813aff75b17f3
huayue <369230782@qq.com>
2024-06-19 11:11:14 +0800

    nvim

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -37,12 +37,19 @@ return {
 				map("v", "<leader>hs", function()
 					gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
 				end, { desc = "暂存更改" })
+				map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "暂存所有更改" })
 
 				map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "取消更改" })
-
 				map("v", "<leader>hr", function()
 					gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
 				end, { desc = "取消更改" })
+				map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "取消所有更改" })
+
+				map("n", "<leader>hu", gitsigns.undo_stage_hunk, { desc = "撤销上次更改" })
+
+				map("n", "<leader>hb", function()
+					gitsigns.blame_line({ full = true })
+				end, { desc = "查看提交信息" })
 
 				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "选中hunk" })
 			end,
