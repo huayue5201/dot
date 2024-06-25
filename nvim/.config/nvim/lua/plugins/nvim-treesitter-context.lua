515bebfb182e5637255f74e4a2256432c2c69383
huayue <369230782@qq.com>
2024-06-25 13:54:49 +0800

    nvim

--- nvim/.config/nvim/lua/plugins/nvim-treesitter-context.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter-context.lua
@@ -3,7 +3,10 @@
 return {
 	"nvim-treesitter/nvim-treesitter-context",
 	config = function()
-		vim.cmd([[hi TreesitterContextLineNumberBottom gui=underline guisp=Grey]])
+		vim.cmd([[
+    " hi TreesitterContextBottom gui=underline guisp=Grey
+    hi TreesitterContextLineNumberBottom gui=underline guisp=Grey
+    ]])
 		require("treesitter-context").setup({
 			enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
 			max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
