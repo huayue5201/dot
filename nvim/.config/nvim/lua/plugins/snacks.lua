a719df5c2cb8e2e65b2e42d96cfb1971df2c372f
huayue <369230782@qq.com>
2024-11-24 00:18:46 +0800

    nvim

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,26 @@
+-- https://github.com/folke/snacks.nvim
+
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		bigfile = { enabled = true },
+		dashboard = { enabled = true },
+		notifier = {
+			enabled = true,
+			timeout = 3000,
+		},
+		quickfile = { enabled = true },
+		statuscolumn = { enabled = true },
+		words = { enabled = true },
+		styles = {
+			notification = {
+				wo = { wrap = true }, -- Wrap notifications
+			},
+		},
+	},
+}
