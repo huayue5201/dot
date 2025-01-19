f5d93a69ce76a87035f2a74eb8778fdfaf4864f9
huayue <369230782@qq.com>
2025-01-19 18:57:07 +0800

    nvim

--- nvim/.config/nvim/lua/plugins/arrow.lua
+++ nvim/.config/nvim/lua/plugins/arrow.lua
@@ -7,10 +7,16 @@ return {
     -- or if using `mini.icons`
     -- { "echasnovski/mini.icons" },
   },
-  keys = { ";", "m" },
-  opts = {
-    show_icons = true,
-    leader_key = ';',        -- Recommended to be a single key
-    buffer_leader_key = 'm', -- Per Buffer Mappings
-  }
+  keys = { ";", "m", "H", "L" },
+  config = function()
+    require('arrow').setup({
+      show_icons = true,
+      leader_key = ';',      -- Recommended to be a single key
+      buffer_leader_key = 'm', -- Per Buffer Mappings
+    })
+
+    vim.keymap.set("n", "H", require("arrow.persist").previous)
+    vim.keymap.set("n", "L", require("arrow.persist").next)
+    -- vim.keymap.set("n", "<C-s>", require("arrow.persist").toggle)
+  end
 }
