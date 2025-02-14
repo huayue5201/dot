b3f1be18ba71a352ab1b5ebe8417ff051d01e504
huayue <369230782@qq.com>
2025-02-14 14:08:36 +0800

    nvim

--- nvim/.config/nvim/lua/plugins/nvim-bqf.lua
+++ nvim/.config/nvim/lua/plugins/nvim-bqf.lua
@@ -1,5 +1,20 @@
 -- https://github.com/kevinhwang91/nvim-bqf
 
 vim.g.later(function()
-	vim.g.add({ source = "kevinhwang91/nvim-bqf" })
+	vim.g.add({
+		source = "nvim-bqf",
+		depends = {
+			"junegunn/fzf",
+			"nvim-treesitter/nvim-treesitter",
+		},
+	})
+
+	vim.g.add({
+		source = "junegunn/fzf",
+		hooks = {
+			post_checkout = function()
+				vim.fn["fzf#install"]()
+			end,
+		},
+	})
 end)
