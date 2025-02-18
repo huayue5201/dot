4f9025b71b80a97ddbfcba08965fadc36cc96df9
huayue <369230782@qq.com>
2025-02-19 01:42:32 +0800

    nvim

--- nvim/.config/nvim/lua/config/autocmds_and_commands.lua
+++ nvim/.config/nvim/lua/config/autocmds_and_commands.lua
@@ -41,6 +41,21 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+-- vim.api.nvim_create_autocmd("LspProgress", {
+-- 	---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+-- 	callback = function(ev)
+-- 		local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+-- 		vim.notify(vim.lsp.status(), "info", {
+-- 			id = "lsp_progress",
+-- 			title = "LSP Progress",
+-- 			opts = function(notif)
+-- 				notif.icon = ev.data.params.value.kind == "end" and " "
+-- 					or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+-- 			end,
+-- 		})
+-- 	end,
+-- })
+
 -- 在特定文件类型中用 q 关闭窗口
 vim.api.nvim_create_autocmd("FileType", {
 	desc = "用q关闭窗口",
