5a818554f6acb35ebe38be30d724e3db304d4c94
huayue <369230782@qq.com>
2025-02-22 00:20:27 +0800

    nvim

--- nvim/.config/nvim/lua/config/statucolumn.lua
+++ nvim/.config/nvim/lua/config/statucolumn.lua
@@ -1,9 +1,7 @@
 local Statuscolumn = {}
 
 -- 获取标记的函数
-local M = {}
-
-function M.get_mark(buf, lnum)
+local function get_mark(buf, lnum)
 	local marks = vim.fn.getmarklist(buf)
 	vim.list_extend(marks, vim.fn.getmarklist())
 	for _, mark in ipairs(marks) do
@@ -12,10 +10,9 @@ function M.get_mark(buf, lnum)
 		end
 	end
 end
-
 -- 显示标记符号配置
 Statuscolumn.mark = function()
-	local mark_info = M.get_mark(vim.api.nvim_get_current_buf(), vim.v.lnum)
+	local mark_info = get_mark(vim.api.nvim_get_current_buf(), vim.v.lnum)
 	if mark_info then
 		-- 如果当前行有标记，返回标记符号和高亮组
 		return "%#" .. mark_info.texthl .. "#" .. mark_info.text
@@ -26,7 +23,7 @@ end
 -- 设置拼接的内容
 Statuscolumn.active = function()
 	return table.concat({
-		" %s",
+		"%s",
 		"%=", -- 自动分隔（左右对齐）
 		"%l",
 		"%=", -- 自动分隔（左右对齐）
