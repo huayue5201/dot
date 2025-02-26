4638333a0c159456e21a13fd43bbf6b66e6b677e
huayue <369230782@qq.com>
2025-02-27 07:32:49 +0800

    nvim

--- nvim/.config/nvim/plugin/colorscheme.lua
+++ nvim/.config/nvim/plugin/colorscheme.lua
@@ -2,9 +2,7 @@
 
 vim.g.now(function()
 	vim.g.add({ source = "oxfist/night-owl.nvim" })
-	-- vim.g.add({ source = "dgox16/oldworld.nvim" })
 
-	require("night-owl").setup()
+	-- require("night-owl").setup()
 	vim.cmd.colorscheme("night-owl")
-	-- vim.cmd.colorscheme("oldworld")
 end)
