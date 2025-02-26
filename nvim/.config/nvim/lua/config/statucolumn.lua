a75177622eab85db7b2b2c1126f94909e727724c
huayue <369230782@qq.com>
2025-02-27 00:36:43 +0800

    nvim

--- nvim/.config/nvim/lua/config/statucolumn.lua
+++ nvim/.config/nvim/lua/config/statucolumn.lua
@@ -1,22 +1,28 @@
 local Statuscolumn = {}
 
--- 获取标记的函数
-vim.cmd("highlight RedMark guifg=#FF0000 gui=bold") -- 创建红色高亮组
+-- 创建红色高亮组
+vim.cmd("highlight RedMark guifg=#FF0000 gui=bold")
+
+-- 缓存标记
 local function get_mark(buf, lnum)
-	local marks = vim.fn.getmarklist(buf) -- 获取当前 buffer 的标记
-	vim.list_extend(marks, vim.fn.getmarklist()) -- 获取全局的标记
+	local marks = vim.fn.getmarklist(buf)
+	-- 优化：如果没有标记，直接返回 nil
+	if #marks == 0 then
+		return nil
+	end
+
 	for _, mark in ipairs(marks) do
+		-- 只查找字母标记
 		if mark.pos[1] == buf and mark.pos[2] == lnum and mark.mark:match("[a-zA-Z]") then
 			return { text = mark.mark:sub(2), texthl = "RedMark" }
 		end
 	end
 end
 
--- 显示标记符号配置
+-- 显示标记符号
 Statuscolumn.mark = function()
 	local mark_info = get_mark(vim.api.nvim_get_current_buf(), vim.v.lnum)
 	if mark_info then
-		-- 如果当前行有标记，返回标记符号和红色高亮组
 		return "%#" .. mark_info.texthl .. "#" .. mark_info.text
 	end
 	return ""
