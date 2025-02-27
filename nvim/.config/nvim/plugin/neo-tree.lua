253c248f70091971b5e281713e11e5a7c7c9404d
huayue <369230782@qq.com>
2025-02-27 11:41:43 +0800

    nvim

--- nvim/.config/nvim/plugin/neo-tree.lua
+++ nvim/.config/nvim/plugin/neo-tree.lua
@@ -11,8 +11,21 @@ vim.g.later(function()
 	})
 
 	require("neo-tree").setup({
-		close_if_last_window = true,
-		open_files_do_not_replace_types = { "terminal", "trouble", "qf" },
+		close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+		popup_border_style = "rounded",
+		enable_git_status = true,
+		enable_diagnostics = true,
+		open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
+		open_files_using_relative_paths = false,
+		sort_case_insensitive = false, -- used when sorting files and directories in the tree
+		sort_function = nil, -- use a custom function for sorting files and directories in the tree
+		-- sort_function = function (a,b)
+		--       if a.type == b.type then
+		--           return a.path > b.path
+		--       else
+		--           return a.type > b.type
+		--       end
+		--   end , -- this sorts files and directories descendantly
 		source_selector = {
 			winbar = true,
 			statusline = false,
@@ -104,24 +117,58 @@ vim.g.later(function()
 				enabled = false,
 			},
 		},
+		-- A list of functions, each representing a global custom command
+		-- that will be available in all sources (if not overridden in `opts[source_name].commands`)
+		-- see `:h neo-tree-custom-commands-global`
+		commands = {
+			system_open = function(state)
+				local node = state.tree:get_node()
+				local path = node:get_id()
+
+				-- 获取当前操作系统
+				local os_type = vim.fn.has("macunix") == 1 and "macOS"
+					or vim.fn.has("unix") == 1 and "Linux"
+					or vim.fn.has("win32") == 1 and "Windows"
+					or "Unknown"
+
+				-- 根据操作系统执行相应的打开文件命令
+				if os_type == "macOS" then
+					-- macOS: 使用 "open" 打开文件
+					vim.fn.jobstart({ "open", path }, { detach = true })
+				elseif os_type == "Linux" then
+					-- Linux: 使用 "xdg-open" 打开文件
+					vim.fn.jobstart({ "xdg-open", path }, { detach = true })
+				elseif os_type == "Windows" then
+					-- Windows: 使用 "explorer" 打开文件
+					local lastSlashIndex = path:match("^.+()\\[^\\]*$") -- 匹配最后的斜杠以及之前的部分
+					local p
+					if lastSlashIndex then
+						p = path:sub(1, lastSlashIndex - 1) -- 提取出最后一个斜杠前的路径
+					else
+						p = path -- 如果没有斜杠，使用原始路径
+					end
+					vim.cmd("silent !start explorer " .. p)
+				else
+					-- 未知操作系统类型
+					print("Unsupported OS")
+				end
+			end,
+		},
 		window = {
 			position = "left",
-			width = 45,
+			width = 40,
 			mapping_options = {
 				noremap = true,
 				nowait = true,
 			},
 			mappings = {
-				["<space>"] = {
-					"toggle_node",
-					nowait = true, -- disable `nowait` if you have existing combos starting with this char that you want to use
-				},
+				["O"] = "system_open",
 				["P"] = {
 					"toggle_preview",
 					config = {
-						use_float = true,
-						use_image_nvim = true,
-						title = "文件预览",
+						use_float = false,
+						-- use_image_nvim = true,
+						-- title = 'Neo-tree Preview',
 					},
 				},
 				["<localleader>e"] = function()
@@ -133,35 +180,201 @@ vim.g.later(function()
 				["<localleader>g"] = function()
 					vim.api.nvim_exec2("Neotree focus git_status left", { output = false })
 				end,
-				["O"] = "system_open",
+				["<space>"] = {
+					"toggle_node",
+					nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+				},
+				["<2-LeftMouse>"] = "open",
+				["<cr>"] = "open",
+				["<esc>"] = "cancel", -- close preview or floating neo-tree window
+				-- Read `# Preview Mode` for more information
+				["l"] = "focus_preview",
+				["S"] = "open_split",
+				["s"] = "open_vsplit",
+				-- ["S"] = "split_with_window_picker",
+				-- ["s"] = "vsplit_with_window_picker",
+				["t"] = "open_tabnew",
+				-- ["<cr>"] = "open_drop",
+				-- ["t"] = "open_tab_drop",
+				["w"] = "open_with_window_picker",
+				--["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
+				["C"] = "close_node",
+				-- ['C'] = 'close_all_subnodes',
+				["z"] = "close_all_nodes",
+				--["Z"] = "expand_all_nodes",
+				["a"] = {
+					"add",
+					-- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
+					-- some commands may take optional config options, see `:h neo-tree-mappings` for details
+					config = {
+						show_path = "none", -- "none", "relative", "absolute"
+					},
+				},
+				["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+				["d"] = "delete",
+				["r"] = "rename",
+				["b"] = "rename_basename",
+				["y"] = "copy_to_clipboard",
+				["x"] = "cut_to_clipboard",
+				["p"] = "paste_from_clipboard",
+				["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
+				-- ["c"] = {
+				--  "copy",
+				--  config = {
+				--    show_path = "none" -- "none", "relative", "absolute"
+				--  }
+				--}
+				["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+				["q"] = "close_window",
+				["R"] = "refresh",
+				["?"] = "show_help",
+				["<"] = "prev_source",
+				[">"] = "next_source",
+				["i"] = "show_file_details",
+				-- ["i"] = {
+				--   "show_file_details",
+				--   -- format strings of the timestamps shown for date created and last modified (see `:h os.date()`)
+				--   -- both options accept a string or a function that takes in the date in seconds and returns a string to display
+				--   -- config = {
+				--   --   created_format = "%Y-%m-%d %I:%M %p",
+				--   --   modified_format = "relative", -- equivalent to the line below
+				--   --   modified_format = function(seconds) return require('neo-tree.utils').relative_date(seconds) end
+				--   -- }
+				-- },
 			},
 		},
-		commands = {
-			system_open = function(state)
-				local node = state.tree:get_node()
-				local path = node:get_id()
+		nesting_rules = {},
+		filesystem = {
+			filtered_items = {
+				visible = false, -- when true, they will just be displayed differently than normal items
+				hide_dotfiles = true,
+				hide_gitignored = true,
+				hide_hidden = true, -- only works on Windows for hidden files/directories
+				hide_by_name = {
+					--"node_modules"
+				},
+				hide_by_pattern = { -- uses glob style patterns
+					--"*.meta",
+					--"*/src/*/tsconfig.json",
+				},
+				always_show = { -- remains visible even if other settings would normally hide it
+					--".gitignored",
+				},
+				always_show_by_pattern = { -- uses glob style patterns
+					--".env*",
+				},
+				never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+					--".DS_Store",
+					--"thumbs.db"
+				},
+				never_show_by_pattern = { -- uses glob style patterns
+					--".null-ls_*",
+				},
+			},
+			follow_current_file = {
+				enabled = false, -- This will find and focus the file in the active buffer every time
+				--               -- the current file is changed while the tree is open.
+				leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+			},
+			group_empty_dirs = false, -- when true, empty folders will be grouped together
+			hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
+			-- in whatever position is specified in window.position
+			-- "open_current",  -- netrw disabled, opening a directory opens within the
+			-- window like netrw would, regardless of window.position
+			-- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
+			use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
+			-- instead of relying on nvim autocmd events.
+			window = {
+				mappings = {
+					["<bs>"] = "navigate_up",
+					["."] = "set_root",
+					["H"] = "toggle_hidden",
+					["/"] = "fuzzy_finder",
+					["D"] = "fuzzy_finder_directory",
+					["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
+					-- ["D"] = "fuzzy_sorter_directory",
+					["f"] = "filter_on_submit",
+					["<c-x>"] = "clear_filter",
+					["[g"] = "prev_git_modified",
+					["]g"] = "next_git_modified",
+					["o"] = {
+						"show_help",
+						nowait = false,
+						config = { title = "Order by", prefix_key = "o" },
+					},
+					["oc"] = { "order_by_created", nowait = false },
+					["od"] = { "order_by_diagnostics", nowait = false },
+					["og"] = { "order_by_git_status", nowait = false },
+					["om"] = { "order_by_modified", nowait = false },
+					["on"] = { "order_by_name", nowait = false },
+					["os"] = { "order_by_size", nowait = false },
+					["ot"] = { "order_by_type", nowait = false },
+					-- ['<key>'] = function(state) ... end,
+				},
+				fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+					["<down>"] = "move_cursor_down",
+					["<C-n>"] = "move_cursor_down",
+					["<up>"] = "move_cursor_up",
+					["<C-p>"] = "move_cursor_up",
+					["<esc>"] = "close",
+					-- ['<key>'] = function(state, scroll_padding) ... end,
+				},
+			},
 
-				-- 根据操作系统决定使用哪个命令
-				if vim.fn.has("mac") == 1 then
-					-- macOS: 使用 open 命令
-					vim.fn.jobstart({ "open", path }, { detach = true })
-				elseif vim.fn.has("unix") == 1 then
-					-- Linux: 使用 xdg-open 命令
-					vim.fn.jobstart({ "xdg-open", path }, { detach = true })
-				elseif vim.fn.has("win32") == 1 then
-					-- Windows: 使用 explorer 打开文件
-					local p
-					local lastSlashIndex = path:match("^.+()\\[^\\]*$") -- 匹配最后一个斜杠之前的路径
-					if lastSlashIndex then
-						p = path:sub(1, lastSlashIndex - 1) -- 提取路径
-					else
-						p = path -- 如果没有斜杠，则使用原路径
-					end
-					vim.cmd("silent !start explorer " .. p)
-				else
-					print("Unsupported OS")
-				end
-			end,
+			commands = {}, -- Add a custom command or override a global one using the same function name
+		},
+		buffers = {
+			follow_current_file = {
+				enabled = true, -- This will find and focus the file in the active buffer every time
+				--              -- the current file is changed while the tree is open.
+				leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+			},
+			group_empty_dirs = true, -- when true, empty folders will be grouped together
+			show_unloaded = true,
+			window = {
+				mappings = {
+					["d"] = "buffer_delete",
+					["bd"] = "buffer_delete",
+					["<bs>"] = "navigate_up",
+					["."] = "set_root",
+					["o"] = {
+						"show_help",
+						nowait = false,
+						config = { title = "Order by", prefix_key = "o" },
+					},
+					["oc"] = { "order_by_created", nowait = false },
+					["od"] = { "order_by_diagnostics", nowait = false },
+					["om"] = { "order_by_modified", nowait = false },
+					["on"] = { "order_by_name", nowait = false },
+					["os"] = { "order_by_size", nowait = false },
+					["ot"] = { "order_by_type", nowait = false },
+				},
+			},
+		},
+		git_status = {
+			window = {
+				position = "float",
+				mappings = {
+					["A"] = "git_add_all",
+					["gu"] = "git_unstage_file",
+					["ga"] = "git_add_file",
+					["gr"] = "git_revert_file",
+					["gc"] = "git_commit",
+					["gp"] = "git_push",
+					["gg"] = "git_commit_and_push",
+					["o"] = {
+						"show_help",
+						nowait = false,
+						config = { title = "Order by", prefix_key = "o" },
+					},
+					["oc"] = { "order_by_created", nowait = false },
+					["od"] = { "order_by_diagnostics", nowait = false },
+					["om"] = { "order_by_modified", nowait = false },
+					["on"] = { "order_by_name", nowait = false },
+					["os"] = { "order_by_size", nowait = false },
+					["ot"] = { "order_by_type", nowait = false },
+				},
+			},
 		},
 		event_handlers = {
 			{
@@ -179,6 +392,5 @@ vim.g.later(function()
 			},
 		},
 	})
-
 	vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>", { desc = "文件树" })
 end)
