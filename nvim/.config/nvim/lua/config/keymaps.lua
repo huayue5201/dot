f5691270900b48557eca7b71d4cf6bc56ab1afff
huayue <369230782@qq.com>
2025-02-28 01:57:09 +0800

    nvim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -7,9 +7,27 @@ vim.keymap.set("n", "dd", function()
 	return vim.fn.getline(".") == "" and '"_dd' or "dd"
 end, { expr = true, desc = "删除当前行（空行使用黑洞寄存器）" })
 
-vim.keymap.set("n", "<leader>yp", ':let @+ = expand("%:p")<cr>', { desc = "复制文件的绝对路径" })
-vim.keymap.set("n", "<leader>yf", ':let @+ = expand("%:f")<cr>', { desc = "复制文件的相对路径" })
-vim.keymap.set("n", "<leader>yt", ':let @+ = expand("%:t")<cr>', { desc = "复制文件名" })
+-- vim.keymap.set("n", "<leader>yp", ':let @+ = expand("%:p")<cr>', { desc = "复制文件的绝对路径" })
+-- vim.keymap.set("n", "<leader>yf", ':let @+ = expand("%:f")<cr>', { desc = "复制文件的相对路径" })
+-- vim.keymap.set("n", "<leader>yt", ':let @+ = expand("%:t")<cr>', { desc = "复制文件名" })
+
+vim.keymap.set("n", "<leader>yp", function()
+	local path = vim.fn.expand("%:p")
+	vim.notify("绝对路径: " .. path, vim.log.levels.INFO)
+	vim.fn.setreg("+", path) -- 将路径复制到剪贴板
+end, { desc = "复制文件的绝对路径" })
+
+vim.keymap.set("n", "<leader>yf", function()
+	local path = vim.fn.expand("%:f")
+	vim.notify("相对路径: " .. path, vim.log.levels.INFO)
+	vim.fn.setreg("+", path) -- 将路径复制到剪贴板
+end, { desc = "复制文件的相对路径" })
+
+vim.keymap.set("n", "<leader>yt", function()
+	local path = vim.fn.expand("%:t")
+	vim.notify("文件名: " .. path, vim.log.levels.INFO)
+	vim.fn.setreg("+", path) -- 将文件名复制到剪贴板
+end, { desc = "复制文件名" })
 
 vim.keymap.set("n", "crc", "*``cgn", { desc = "修改当前选中文本" })
 
