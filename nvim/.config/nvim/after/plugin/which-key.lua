d2bca5c43056f8433c9d7a4c18f98385570fa9c5
huayue <369230782@qq.com>
2025-02-28 23:51:49 +0800

    nvim

--- nvim/.config/nvim/after/plugin/which-key.lua
+++ nvim/.config/nvim/after/plugin/which-key.lua
@@ -1,7 +1,13 @@
+-- https://github.com/folke/which-key.nvim
+
 vim.g.later(function()
 	vim.g.add({ source = "folke/which-key.nvim" })
 
 	require("which-key").setup({
 		preset = "helix",
 	})
+
+	vim.keymap.set("n", "<leader>?", function()
+		require("which-key").show({ global = false })
+	end, { desc = "Buffer Local Keymaps (which-key)" })
 end)
